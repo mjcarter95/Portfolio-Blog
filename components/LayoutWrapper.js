f21be3a9f5ac5ec7b5fcae8936da71b9f6913fb0
Matthew Carter <mjcarter95@gmail.com>
2023-05-22 18:45:21 +0100

    logo and footer

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -18,13 +18,7 @@ const LayoutWrapper = ({ children }) => {
                 {/* <div className="mr-3">
                   <Logo />
                 </div> */}
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
+                <div className="h-6 text-2xl font-bold">{siteMetadata.author}</div>
               </div>
             </Link>
           </div>
