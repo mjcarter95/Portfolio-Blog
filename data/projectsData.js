ca0e261ccd11b4f6e8f2f89e5e6760cc3b123568
Matthew Carter <m.j.carter2@liverpool.ac.uk>
2023-06-25 22:08:51 +0100

    tweak projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -13,7 +13,7 @@ const projectsData = [
     title: 'CondorCMF',
     featured: true,
     type: 'Python Package',
-  date: '2023-05-15',
+  date: '2023-06-25',
     description: `A framework for implementing coordinator-follower and coordinator-manager-follower networks on vanilla HTCondor.`,
     imgSrc: '/static/images/projects/condorcmf.png',
     href: 'https://github.com/mjcarter95/CondorCMF',
@@ -58,7 +58,7 @@ const projectsData = [
   // },
   {
     title: 'Portfolio Website',
-    featured: false,
+    featured: true,
     type: 'Next.js and Tailwind CSS',
     date: '2023-05-23',
     description: `The source code for this website, built using Node.js and Tailwind CSS. Based on the Tailwind Next.js Starter Blog by Timothy Lin.`,
